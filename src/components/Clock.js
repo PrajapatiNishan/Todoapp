07509f49c2633ae99697f1b0325cef7cbf55200a
NishanPrajapati <119466701+PrajapatiNishan@users.noreply.github.com>
2023-05-27 11:24:24 +0545

    updated clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
 
 export const Clock = () =>{
-    let time = new Date().toLocaleDateString();
+    let time = new Date().toLocaleTimeString();
     const   [currentTime, setCurrentTime] = useState(time);
+
+    const updateTime = () => {
+        let time = new Date().toLocaleTimeString();
+        setCurrentTime(time);
+    } 
+
+    setInterval(updateTime, 1000);
+
     return(
         <div>
             <h2>{currentTime}</h2>
